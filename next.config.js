072d515ef877a3c9a0aad0dae6841b3ed829b7ce
mohamed-dellai <mohameddellai6@gmail.com>
2025-11-02 11:02:03 +0100

    last fix for today

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
-
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
-  // Optimisation pour Vercel
   poweredByHeader: false,
-  // Optimisation des images
-  images: {
-    domains: [],
-    unoptimized: process.env.NODE_ENV !== 'production',
-  },
-  // Add Webpack configuration for the Prisma Plugin
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(new PrismaPlugin());
-    }
-    // Important: return the modified config
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
