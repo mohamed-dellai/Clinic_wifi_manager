5a2e627dc18867c48b34ee1e6be61f2931f914ea
mohamed-dellai <mohameddellai6@gmail.com>
2025-11-02 10:07:48 +0100

    commit changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
-    appDir: true,
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+  // Optimisation pour Vercel
+  poweredByHeader: false,
+  // Optimisation des images
+  images: {
+    domains: [],
+    unoptimized: process.env.NODE_ENV !== 'production',
   },
 };
